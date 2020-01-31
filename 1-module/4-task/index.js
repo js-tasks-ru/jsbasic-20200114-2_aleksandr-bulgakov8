028eebc2fc2a5c25427cc0e2a7e8402f36460d5e
Sn-zz-owO-zz-wl <keffear@gmail.com>
2020-01-31 23:24:02 +0500

    Задача на спам

---
+++ 1-module/4-task/index.js
@@ -0,0 +1,11 @@
+/**
+ * checkSpam
+ * @param {string} str base
+ * @returns {boolean}
+ */
+function checkSpam(str) {
+str = str.toLowerCase();
+  return str.includes('1xbet') || str.includes('xxx');
+}
+
+
