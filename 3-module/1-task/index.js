f6a576158612f69138cb32f18c40af4f426e2894
Sn-zz-owO-zz-wl <keffear@gmail.com>
2020-02-07 23:45:13 +0500

    Delete index.js

--- 3-module/1-task/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * showSalary
- * @param {Array} data - данные о пользователях
- * @param {number} age - максимальный возраст
- * @returns {string}
- */
-function showSalary(data, age) {
-  return data.filter(item => item.age <= age).map(item => `${item.name}, ${item.balance}`).join(`\n`);
-}
